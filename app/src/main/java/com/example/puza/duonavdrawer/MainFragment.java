fb6334988f9c209ab463c15ba73103f1eea0ac43
puja-shrestha <shresthaa.puza@gmail.com>
2018-08-30 14:19:25 +0545

    NAvigation Drawer added

---
+++ app/src/main/java/com/example/puza/duonavdrawer/MainFragment.java
@@ -0,0 +1,76 @@
+package com.example.puza.duonavdrawer;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ViewPager pager = (ViewPager) view.findViewById(R.id.pager);
+        pager.setAdapter(new PagerAdapter());
+        return view;
+    }
+
+    private class PagerAdapter extends android.support.v4.view.PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return 6;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+
+            // Create some layout params
+            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT);
+            layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+
+            // Create some text
+            TextView textView = getTextView(container.getContext());
+            textView.setText(String.valueOf(position));
+            textView.setLayoutParams(layoutParams);
+
+            RelativeLayout layout = new RelativeLayout(container.getContext());
+            layout.setBackgroundColor(ContextCompat.getColor(container.getContext(), R.color.colorWhite));
+            layout.setLayoutParams(layoutParams);
+
+            layout.addView(textView);
+            container.addView(layout);
+            return layout;
+        }
+
+        private TextView getTextView(Context context) {
+            TextView textView = new TextView(context);
+            textView.setTextColor(Color.BLACK);
+            textView.setTextSize(30);
+            textView.setTypeface(Typeface.DEFAULT_BOLD);
+            return textView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((RelativeLayout) object);
+        }
+    }
+}
